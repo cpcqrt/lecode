b4d0bbae2d5f028dc6e753008da2a29c706764a9
ChangPao Chang(張長葆) <NVT03529@novatek.com.tw>
2016-11-30 19:01:55 +0800

    Add lecode double vector print

--- myVectorFunc.cpp
+++ myVectorFunc.cpp
@@ -3,15 +3,30 @@
 
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+	os << "[ ";
 	for(auto const& c : v) os << c << ' ';
-	os << std::endl;
+	os << "]" << std::endl;
+	return os;
+	
+	// faster ?
+	// void print(const std::vector<T>& v){
+		// std::copy(v.begin(), v.end(), std::ostream_iterator<T>(std::cout, " "));
+		// std::cout << std::endl;
+	// }
+}
+
+// Double vector print, follow leetcode convention
+// Although Single vector print also work, but less pretty
+template <typename T>
+std::ostream& operator<<(
+	std::ostream& os, 
+	const std::vector<std::vector<T> > &vv
+	)
+{
+	os << "[" << std::endl;
+	for(auto const& c : vv) os << "  " << c ;
+	os << "]" << std::endl;
 	return os;
 }
 
 
-
-// faster ?
-// void print(const std::vector<T>& v){
-	// std::copy(v.begin(), v.end(), std::ostream_iterator<T>(std::cout, " "));
-	// std::cout << std::endl;
-// }
