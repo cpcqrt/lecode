49ff7d536d1c7cdbd041745375ebd62d147e9b64
ChangPao Chang(張長葆) <NVT03529@novatek.com.tw>
2016-11-30 20:04:24 +0800

    Add debug_print const string

--- GeneralFunc.cpp
+++ GeneralFunc.cpp
@@ -4,3 +4,8 @@ template <typename T>
 void debug_print(const char* name, T value){
 	std::cout << name << ": "<< value;
 }
+
+// in case DEBUG_PRINT("aaa");
+void debug_print(const char* name, const char* value){
+	std::cout << value << std::endl;
+}
