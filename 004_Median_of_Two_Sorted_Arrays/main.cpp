53dae13082c9b68bac70e51ab554cd800368da77
ChangPao Chang(張長葆) <NVT03529@novatek.com.tw>
2016-11-30 10:25:26 +0800

    Rename the file in lecode 004

--- 004_Median_of_Two_Sorted_Arrays/main.cpp
+++ 004_Median_of_Two_Sorted_Arrays/main.cpp
@@ -20,40 +20,38 @@ double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 	
 	// start counting, using merge sort technique
 	int idx1 = 0, idx2 = 0, idx3 = 0;
-	for (;idx1 < nums1.size();idx3++){
-		// if (idx2 == num2s.size()){
-			// // vector 2 reached end
-			// if (idx3 == idx3_half - 1) val[0] = nums1[idx1];
-			// if (idx3 == idx3_half)     val[1] = nums1[idx1];
-		// }
-		// if (nums1[idx1] <= nums2[idx2]){
-			// if (idx3 == idx3_half_1) val[0] = nums1[idx1];
-			// if (idx3 == idx3_half)   val[1] = nums1[idx1];
-			// idx1 ++;
-		// } else {			
-			// if (idx3 == idx3_half_1) val[0] = nums2[idx2];
-			// if (idx3 == idx3_half)   val[1] = nums2[idx2];
-			// idx2 ++;
-		// }
-	}
-	
-	while ( && idx2 < nums2.size()){
+	bool has_reach = false;
+	while (idx1 < nums1.size() && idx2 < nums2.size()){
 		// filling idx3, while comparing idx1 and idx2
-		
+		if (nums1[idx1] <= nums2[idx2]){
+			if (idx3 == idx3_half_1) val[0] = nums1[idx1];
+			if (idx3 == idx3_half)   val[1] = nums1[idx1];
+			idx1 ++;	idx3 ++;
+		} else {			
+			if (idx3 == idx3_half_1) val[0] = nums2[idx2];
+			if (idx3 == idx3_half)   val[1] = nums2[idx2];
+			idx2 ++;	idx3 ++;
+		}
 		if (idx3 == idx3_half + 1) break;
 	}
+	// cout << val[0] << val[1] << endl;
+	cout << "idx1: " << idx1 << " , idx2: " << idx2 << endl;
+	cout << "idx3: " << idx3 << " , idx3_half: " << idx3_half << endl;
+	
 	// previous loop will end in two condition
-	// 1. already filled to idx3_half
+	// 1. already filled to idx3_half -1 
 	// 2. either vector reached the end
 	if (idx3 < idx3_half + 1){
 		// either vector reached the end
 		if (idx1 == nums1.size()){
-			val[0] = nums2[idx3_half - idx2 - 1];
-			val[1] = nums2[idx3_half - idx2];
+			if (idx3 < idx3_half) 
+				val[0] = nums2[(idx3_half - idx3) + idx2 - 1];
+			val[1] = nums2[(idx3_half - idx3) + idx2];
 		}
 		else if(idx2 == nums2.size()){
-			val[0] = nums1[idx3_half - idx1 - 1];
-			val[1] = nums1[idx3_half - idx1];
+			if (idx3 < idx3_half) 
+				val[0] = nums1[(idx3_half - idx3) + idx1 - 1];
+			val[1] = nums1[(idx3_half - idx3) + idx1];
 		}
 		else{
 			throw "Not gonna happen";
@@ -69,21 +67,28 @@ double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 int main(){
 	// vector<int> v1 {2,4,3,9}; 
 	// vector<int> v2 {5,6,7,3,11}; 
-	vector<int> v1 {1,2}; 
-	vector<int> v2 {3,4}; 
+	
+	// tricky test bench 1
+	// vector<int> v1 {1,2}; 
+	// vector<int> v2 {3,4}; 
+	
+	// tricky test bench 2
+	vector<int> v1 {2}; 
+	vector<int> v2 {1,3,4}; 
 	
 	sort(v1.begin(), v1.end());
 	sort(v2.begin(), v2.end());
-	cout << v1;
-	cout << v2;
+	cout << "v1: " << v1;
+	cout << "v2: " << v2;
 	
 	// Real solution
 	std::vector<int> v3;
     std::merge(v1.begin(), v1.end(), v2.begin(), v2.end(), std::back_inserter(v3));	
-	cout << v3;
+	cout << "v3: " << v3;
 	double median = double(v3[v3.size()/2]);
 	if (v3.size()%2 == 0) median = (median + double(v3[v3.size()/2-1]))/2.0;
 	cout << "Exact median = " << median << endl;
+	cout << "------------------------------" << endl;
 	
 	cout << "  My  median = "
 	     << findMedianSortedArrays(v1,v2) << endl;
