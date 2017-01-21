b367a9ef4ce6d2fb6cb961680d5cf87d7ff48608
cnpvct <evareiasuaka@hotmail.com>
2017-01-21 17:37:10 +0800

    ~ 038 not finished

---
+++ 038_Count_and_Say/main.cpp
@@ -0,0 +1,30 @@
+// Leetcode History
+// 2016/11/30 Beat 62.25%
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include "../GeneralFunc.cpp"
+
+using namespace std;
+
+
+
+
+string countAndSay(int n) {
+	string s = "";
+	
+	return s;
+}
+
+
+int main(){
+	vector<int> array_n = {1, 11, 21, 1211, 111221};
+	
+	for ( auto i : array_n){
+		cout << i << " : " << countAndSay(i) << endl;
+	}
+	
+	
+	return 0;
+}
